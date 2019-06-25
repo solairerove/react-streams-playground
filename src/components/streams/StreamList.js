3c727b2a046b4a29d56fb09d25efc07ab271051a
Mikita Kryvitski <solairerove@gmail.com>
2019-06-25 23:49:13 +0300

    feature: add fetch and delete stream reducers

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamList = () => {
-  return <div>Stream List</div>;
-};
+class StreamList extends React.Component {
+  componentDidMount() {
+    const { getStream } = this.props;
 
-export default StreamList;
+    getStream(1);
+  }
+
+  renderList = () => {
+    const { streams } = this.props;
+    console.log(streams);
+
+    return <div>{streams.length}</div>;
+  };
+
+  render() {
+    return <div>{this.renderList()}</div>;
+  }
+}
+
+const mapStateToProps = state => ({ streams: state.streams });
+
+export default connect(
+  mapStateToProps,
+  {
+    getStream: fetchStream
+  }
+)(StreamList);
