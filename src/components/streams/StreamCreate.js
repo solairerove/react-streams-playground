22ccda2c0ecc4f3206043f99bbb1ec50cc46db13
Mikita Kryvitski <solairerove@gmail.com>
2019-06-25 18:55:54 +0300

    feature: init form and onSubmit handle

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,15 +2,32 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderIntput({ input }) {
-    return <input {...input} />;
+  renderIntput({ input, label }) {
+    return (
+      <div className='field'>
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+  onSubmit(event) {
+    // event.preventDefault() on redux-form side
   }
 
   render() {
     return (
-      <form>
-        <Field name='title' component={this.renderIntput} />
-        <Field name='description' component={this.renderIntput} />
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className='ui form'
+      >
+        <Field name='title' component={this.renderIntput} label='Enter Title' />
+        <Field
+          name='description'
+          component={this.renderIntput}
+          label='Enter Description'
+        />
+        <button className='ui button primary'>Submit</button>
       </form>
     );
   }
