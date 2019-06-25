8e3b3ba311250c3be54e209646124e4f9342182d
Mikita Kryvitski <solairerove@gmail.com>
2019-06-25 19:20:06 +0300

    feature: add render error function

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderIntput({ input, label, meta }) {
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="heaer">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  renderIntput = ({ input, label, meta }) => {
     return (
-      <div className='field'>
+      <div className="field">
         <label>{label}</label>
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValues) {
     // event.preventDefault() on redux-form side
@@ -20,15 +30,15 @@ class StreamCreate extends React.Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className='ui form'
+        className="ui form"
       >
-        <Field name='title' component={this.renderIntput} label='Enter Title' />
+        <Field name="title" component={this.renderIntput} label="Enter Title" />
         <Field
-          name='description'
+          name="description"
           component={this.renderIntput}
-          label='Enter Description'
+          label="Enter Description"
         />
-        <button className='ui button primary'>Submit</button>
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
