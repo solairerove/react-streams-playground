77cf9263ac5da9b6f67cb6a616c45aa969142768
Mikita Kryvitski <solairerove@gmail.com>
2019-06-26 21:51:55 +0300

    feature: add styles and some handle on modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createBrowserHistory from '../history';
 
 const Modal = props => {
+  const handleClose = () => {
+    createBrowserHistory.push('/');
+  };
+
+  const stopPropagation = e => {
+    e.stopPropagation();
+  };
+
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Modal</div>
+    <div onClick={handleClose} className="ui dimmer modals visible active">
+      <div
+        onClick={stopPropagation}
+        className="ui standard modal visible active"
+      >
+        <div className="header">Delete Stream</div>
+        <div className="content">
+          Are you sure you want to delete this stream?
+        </div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </div>
     </div>,
     document.querySelector('#modal')
   );
