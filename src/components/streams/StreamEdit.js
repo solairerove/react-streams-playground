56f0ca303210d7dc30f349b1641ce2d997018183
Mikita Kryvitski <solairerove@gmail.com>
2019-06-26 19:44:39 +0300

    feature: load stream in stream edit component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-  return <div>Stream Edit</div>;
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.getStream(this.props.match.params.id);
+  }
 
-export default StreamEdit;
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    } else {
+      return <div>{this.props.stream.id}</div>;
+    }
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  stream: state.streams[ownProps.match.params.id]
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    getStream: fetchStream
+  }
+)(StreamEdit);
